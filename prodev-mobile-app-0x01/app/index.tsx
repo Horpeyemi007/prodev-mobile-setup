d49588aefb8d4da6bd6683190692f2ec6da6d027
Opeyemi <Opeyemi5055@yahoo.com>
2025-08-27 03:37:20 +0100

    task2

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -30,7 +30,6 @@ const styles = StyleSheet.create({
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Entry Screen - Awesome</Text>
       <Text style={styles.largeText}>
         Typescript is great if you practice more
       </Text>
