5f3e7ae1f44a470832d7fbd2607fe91fa66d253e
Opeyemi <Opeyemi5055@yahoo.com>
2025-08-27 03:44:59 +0100

    task2

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -36,7 +36,7 @@ export default function Index() {
           Typescript is great if you practice more
         </Text>
         <Text style={styles.mediumText}>
-          React Native provides you single codebase for cross platforms
+          React Native provides you a single codebase for cross platforms
         </Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
